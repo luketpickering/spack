4473212a4d01630c9b654f4b14f04a19176d7ad6
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-09-25 15:01:19 +0200

    builtin.mock et al. : changes to packages
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- var/spack/repos/builtin.mock/packages/multimethod/package.py
+++ var/spack/repos/builtin.mock/packages/multimethod/package.py
@@ -26,6 +26,7 @@ class Multimethod(MultimethodBase):
     variant("mpi", default=False, description="")
 
     depends_on("mpi", when="+mpi")
+    depends_on("c", type="build")
 
     #
     # These functions are only valid for versions 1, 3, and 4.
